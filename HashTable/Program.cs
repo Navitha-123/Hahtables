78d2536ee6ea94e4547b1e5deb61d34cfafd068e
navitha <navithareddy112@gmail.com>
2022-09-16 17:09:40 +0530

    [navitha] UC1MyMapNode

---
+++ HashTable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("choose option 1.Add to create mymapnode");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    UC1MyMapNode<string, string> hashtable = new UC1MyMapNode<string, string>(5);
+                    hashtable.Add("0", "To");
+                    hashtable.Add("1", "be");
+                    hashtable.Add("2", "or");
+                    hashtable.Add("3", "not");
+                    hashtable.Add("4", "To");
+                    hashtable.Add("5", "be");
+
+                    Console.WriteLine("enter the key to search the value:");
+                    string key = Console.ReadLine();
+                    string value = hashtable.GetV(key);
+                    Console.WriteLine("{0} index valiue :{1}",key,value);
+                    break;
+                default:
+                    Console.WriteLine("enter wrong choice");
+                    break;
+
+
+
+            }
+
+        }
+    }
+}
---
+++ HashTable/UC1MyMapNode.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+   public class UC1MyMapNode<K, V>
+    {
+        private int size;
+        private LinkedList<KeyValue<K, V>>[] items;
+
+        public UC1MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int pos)
+        {
+            LinkedList<KeyValue<K, V>> linkedlist = items[pos];
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                items[pos] = linkedlist;
+            }
+            return linkedlist;
+        }
+
+        public V GetV(K key)
+        {
+            int pos = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(pos);
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int pos = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(pos);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedlist.AddLast(item);
+        }
+
+        public struct KeyValue<K, V>
+        {
+            public K Key { get; set; }
+            public V Value { get; set; }
+        }
+
+    }
+}
